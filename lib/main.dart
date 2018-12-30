8c50127a50e58d2126556f41f72f89217814dac8
Dreamwalker <aristojeff@gmail.com>
2018-12-30 12:36:48 +0900

    lift off

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter cash"),
+        backgroundColor: Colors.deepOrange,
+        centerTitle: Theme.of(context).platform == TargetPlatform.iOS ? false : false,
+        elevation: 1.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            tooltip: "Add Something",
+            onPressed: (){},
+          )
+        ],
+      ),
+
+      bottomNavigationBar: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
+
+
